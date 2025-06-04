3ff90ecb8feeeb269be38e9d8c773acd19d7a187
AnzinFelipe <ian.felipe.drunen@gmail.com>
2025-06-04 17:57:32 -0300

    Adicionei a função de troca de wallpaper

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ function trocarImg() {
     imgNova = imgAnterior;
     imgAnterior = nova;
 }
+
+function salvar(tema) {
+    document.body.style.backgroundImage = `url(${tema})`;
+    localStorage.setItem("BG", tema);
+}
+
+function aplicar() {
+  const BGimg = localStorage.getItem("BG");
+  if (BGimg) {
+    document.body.style.backgroundImage = `url(${BGimg})`;
+  }
+}
+
+window.addEventListener("load", aplicar);
