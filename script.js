1a8dfd5a9ba7b4931c3334b4438da3a48e2a5ae8
AnzinFelipe <ian.felipe.drunen@gmail.com>
2025-07-18 21:51:47 -0300

    Troquei a funcionalidade dos wallpapers pela troca entre dois temas

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
 let imgNova = "imagens/desenho.png";
 let imgAnterior = "imagens/eu.jpeg";
 
+let tema = document.getElementById("btnTema");
+
+let body = document.querySelector("body");
+
+tema.addEventListener("click", () => {
+  tema.classList.toggle("novoTema");
+  body.classList.toggle("novoTema");
+})
+
 function trocarImg() {
     document.getElementById("eu").src = imgNova;
     let nova = imgNova;
     imgNova = imgAnterior;
     imgAnterior = nova;
 }
-
-function salvar(tema) {
-    document.body.style.backgroundImage = `url(${tema})`;
-    localStorage.setItem("BG", tema);
-}
-
-function aplicar() {
-  const BGimg = localStorage.getItem("BG");
-  if (BGimg) {
-    document.body.style.backgroundImage = `url(${BGimg})`;
-  }
-}
-
-window.addEventListener("load", aplicar);
